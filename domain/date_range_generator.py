d35b193ea81108825efb8941269a17e781be686f
encheladus <213512378+encheladus@users.noreply.github.com>
2026-02-21 13:29:23 +0100

    Add DateRangeGenerator for generating date ranges
    Implement DateRangeGenerator class to generate dates between start and end dates.

---
+++ domain/date_range_generator.py
@@ -0,0 +1,16 @@
+from datetime import date, timedelta
+
+class DateRangeGenerator:
+    @staticmethod
+    def generate_departure_dates(start_date, end_date):
+        if start_date > end_date:
+            raise ValueError("start_date cannot be after end_date")
+        dates_list = []
+        current_date = start_date
+        while current_date <= end_date:
+            dates_list.append(current_date)
+            current_date += timedelta(days=1)
+        # Replacing the while loop by a for loop 
+        # delta = (end_date - start_date).days
+        # return [start_date + timedelta(days=i) for i in range(delta + 1)]
+    return dates_list
