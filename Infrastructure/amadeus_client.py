0e8aed4249b3d4d240b0f333147c958eb764830b
encheladus <213512378+encheladus@users.noreply.github.com>
2026-03-07 13:02:00 +0000

    Adding error management layer in class AmadeusClient

--- Infrastructure/amadeus_client.py
+++ Infrastructure/amadeus_client.py
@@ -6,10 +6,15 @@ class AmadeusClient:
 
     def __init__(self):
         load_dotenv()
+        api_key = client_id = os.getenv("AMADEUS_API_KEY"),
+        api_secret = client_secret = os.getenv("AMADEUS_API_SECRET")
+
+        if not api_key or not api_secret:
+            raise ValueError("The Amadeus API keys are not set in .env")
 
         self.client = Client(
-            client_id=os.getenv("AMADEUS_API_KEY"),
-            client_secret=os.getenv("AMADEUS_API_SECRET")
+            client_id=api_key,
+            client_secret=api_secret
         )
 
     def get_client(self):
