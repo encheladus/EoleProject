593dc3b11fca1b4fdb6e04d5eb9ce3e72355d85b
Nicolas Seyer <213512378+encheladus@users.noreply.github.com>
2026-05-02 17:31:06 +0200

    Adding the db layer to the main in order to record the searches and results

--- interfaces/cli/main.py
+++ interfaces/cli/main.py
@@ -8,11 +8,16 @@ from domain.cheapest_trip_finder import CheapestTripFinder
 
 from application.flight_search_service import FlightSearchService
 from application.search_result_displayer import format_cheapest_trips
+from application.search_mapping import map_search
 
 from infrastructure.serp_api.serp_api_client import SerpApiClient
 from infrastructure.serp_api.serp_api_flight_provider import SerpApiFlightProvider
+from infrastructure.historic.flight_search_database import FlightSearchDatabase
 
 load_dotenv()
+
+#Initiate my DB
+db = FlightSearchDatabase()
 
 # I first need the starting date, the trip duration, and the search period:
 start_date_str = input("When do you want to go? (YYYY-MM-DD) ")
@@ -37,14 +42,18 @@ trip_combinations = combination_generator.generate(possible_departures)
 origin = input("Departure airport code: ")
 destination = input("Arrival airport code: ")
 
+mapped_search = map_search(origin, destination, stay_duration_days, search_period_days)
+search_id = db.insert_search(mapped_search)
+
 trips = TravelFormatter.format_combinations(trip_combinations, origin, destination)
 
 # Finally, I use my FlightSearchService:
 serp_api_client = SerpApiClient()
 serp_api_provider = SerpApiFlightProvider(serp_api_client)
-flight_service = FlightSearchService(serp_api_provider, cache=cache)
+flight_service = FlightSearchService(serp_api_provider)
 
 trips_with_prices = flight_service.search_flight(trips)
 cheapest_trip, average_price = CheapestTripFinder.find_cheapest(trips_with_prices)
+db.insert_result(cheapest_trip, search_id, "serp_api")
 
 print(format_cheapest_trips(cheapest_trip, average_price))
